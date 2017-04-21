73a8317374613c54f50922fc0a7463423edc72be
王东杰 <wangdongjie@cvte.com>
2017-04-21 21:49:37 +0800

    评分的table中每个表格添加特殊的名称。

--- MarkingSystem/routes/index.js
+++ MarkingSystem/routes/index.js
@@ -1,24 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var whoami = "";
+var users = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞'];
+var title = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞', '总分', '平均分'];
+
+var markingtitle = ['姓名', '工作项评分', '专利', '小零识', '导师', '定制版', '招聘', '内推', '培训','服务生','特殊加分'];
+var markingtitlesen = ['xingming', 'gongzuoxiang', 'zhuanli', 'xiaolingshi', 'daoshi', 'dingzhiban', 'zhaopin', 'neitui', 'peixun','fuwusheng','teshujiafen'];
+var usersen = ['chenbo','chenminjing','chentianlong','chengyao','dengqiliang','fujinxiang','guyichang','lixingbin','lvyi','wangdongjie','xuwei','yehong','zhangqiang','zhulongfei'];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {  
+      users: users,
+      usersen: usersen, });
 });
 
 /* GET marking page. */
 router.get('/marking.html', function (req, res, next) {
-  res.render('marking', { title: 'Express' });
+  res.render('marking', {
+      users: users,
+      scores: scores, 
+      usersen: usersen,
+      markingtitle: markingtitle, 
+      markingtitlesen: markingtitlesen,
+    });
 });
 
 /* POST marking page. */
 router.post('/marking.html', function (req, res, next) {
   res.cookie('whoami', req.body.whoami);
-  res.render('marking', { title: 'Express' });
+  res.render('marking', {
+      whoami: req.body.whoami,
+      users: users,
+      scores: scores, 
+      usersen: usersen,
+      markingtitle: markingtitle,
+      markingtitlesen: markingtitlesen,
+    });
 });
-
-var users = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞'];
-var title = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞', '总分', '平均分'];
 
 var scores = new Array;
 for (var i = 0; i < users.length; i++) {
@@ -29,22 +49,26 @@ for (var i = 0; i < users.length; i++) {
 }
 
 router.post('/statistics.html', function (req, res, next) {
-  var index = users.indexOf(req.cookies.whoami);
+  var index = usersen.indexOf(req.cookies.whoami);
   if (index != -1) {
-    scores[0][index] = CheckScore(req.body.chenbo);
-    scores[1][index] = CheckScore(req.body.chenminjing);
-    scores[2][index] = CheckScore(req.body.chentianlong);
-    scores[3][index] = CheckScore(req.body.chengyao);
-    scores[4][index] = CheckScore(req.body.dengqiliang);
-    scores[5][index] = CheckScore(req.body.fujinxiang);
-    scores[6][index] = CheckScore(req.body.guyichang);
-    scores[7][index] = CheckScore(req.body.lixingbin);
-    scores[8][index] = CheckScore(req.body.lvyi);
-    scores[9][index] = CheckScore(req.body.wangdongjie);
-    scores[10][index] = CheckScore(req.body.xuwei);
-    scores[11][index] = CheckScore(req.body.yehong);
-    scores[12][index] = CheckScore(req.body.zhangqiang);
-    scores[13][index] = CheckScore(req.body.zhulongfei);
+    usersen.forEach(function(useren,i) {
+      var gongzuoxiang = req.cookies.whoami+"_"+useren+"_"+"gongzuoxiang";
+      scores[i][index] = CheckScore(req.body[gongzuoxiang]);
+    }, this);
+    
+    // scores[1][index] = CheckScore(req.body.chenminjing);
+    // scores[2][index] = CheckScore(req.body.chentianlong);
+    // scores[3][index] = CheckScore(req.body.chengyao);
+    // scores[4][index] = CheckScore(req.body.dengqiliang);
+    // scores[5][index] = CheckScore(req.body.fujinxiang);
+    // scores[6][index] = CheckScore(req.body.guyichang);
+    // scores[7][index] = CheckScore(req.body.lixingbin);
+    // scores[8][index] = CheckScore(req.body.lvyi);
+    // scores[9][index] = CheckScore(req.body.wangdongjie);
+    // scores[10][index] = CheckScore(req.body.xuwei);
+    // scores[11][index] = CheckScore(req.body.yehong);
+    // scores[12][index] = CheckScore(req.body.zhangqiang);
+    // scores[13][index] = CheckScore(req.body.zhulongfei);
     Statistic();
   }
   Load(res);
@@ -56,7 +80,7 @@ router.get('/statistics.html', function (req, res, next) {
 
 function Load(res) {
   var finishusercount = CountFinishUser();
-  if (CountFinishUser() >= 5) {
+  if (CountFinishUser() >= 1) {
     res.render('statistics', {
       title: title,
       users: users,
