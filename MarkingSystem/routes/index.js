cb6777ff59b72bc2bec0f8f2a61e7f1cb2d4541d
王东杰 <wangdongjie@cvte.com>
2017-04-21 16:37:22 +0800

    实现table的排序，添加正在统计中的进度显示。

--- MarkingSystem/routes/index.js
+++ MarkingSystem/routes/index.js
@@ -2,126 +2,104 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET marking page. */
-router.get('/marking.html', function(req, res, next) {
+router.get('/marking.html', function (req, res, next) {
   res.render('marking', { title: 'Express' });
 });
 
 /* POST marking page. */
-router.post('/marking.html', function(req, res, next) {
-  res.cookie('whoami',req.body.whoami);
+router.post('/marking.html', function (req, res, next) {
+  res.cookie('whoami', req.body.whoami);
   res.render('marking', { title: 'Express' });
 });
 
-var users = ['陈博','陈民敬','陈天龙','程遥','邓启亮','付金祥','古意昌','李幸斌','吕毅','王东杰','许威','叶洪','张强','朱龙飞'];
-var statisticscoretitles = ['部分','平均分'];
+var users = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞'];
+var title = ['陈博', '陈民敬', '陈天龙', '程遥', '邓启亮', '付金祥', '古意昌', '李幸斌', '吕毅', '王东杰', '许威', '叶洪', '张强', '朱龙飞', '总分', '平均分'];
 
 var scores = new Array;
-for(var i=0;i<users.length;i++){
-  scores[i]=new Array();
-  for(var j=0;j<users.length;j++){
-    scores[i][j]=0;
+for (var i = 0; i < users.length; i++) {
+  scores[i] = new Array();
+  for (var j = 0; j < title.length; j++) {
+    scores[i][j] = 0;
   }
 }
 
-var statisticscores = new Array;
-for(var i=0;i<users.length;i++){
-  statisticscores[i]=new Array();
-  for(var j=0;j<2;j++){
-    statisticscores[i][j]=0;
+router.post('/statistics.html', function (req, res, next) {
+  var index = users.indexOf(req.cookies.whoami);
+  if (index != -1) {
+    scores[0][index] = CheckScore(req.body.chenbo);
+    scores[1][index] = CheckScore(req.body.chenminjing);
+    scores[2][index] = CheckScore(req.body.chentianlong);
+    scores[3][index] = CheckScore(req.body.chengyao);
+    scores[4][index] = CheckScore(req.body.dengqiliang);
+    scores[5][index] = CheckScore(req.body.fujinxiang);
+    scores[6][index] = CheckScore(req.body.guyichang);
+    scores[7][index] = CheckScore(req.body.lixingbin);
+    scores[8][index] = CheckScore(req.body.lvyi);
+    scores[9][index] = CheckScore(req.body.wangdongjie);
+    scores[10][index] = CheckScore(req.body.xuwei);
+    scores[11][index] = CheckScore(req.body.yehong);
+    scores[12][index] = CheckScore(req.body.zhangqiang);
+    scores[13][index] = CheckScore(req.body.zhulongfei);
+    Statistic();
   }
-}
-
-router.post('/statistics.html', function(req, res, next) {
-  var index=users.indexOf(req.cookies.whoami);
-  if(index!=-1){
-  scores[0][index]=CheckScore(req.body.chenbo);
-  scores[1][index]=CheckScore(req.body.chenminjing);
-  scores[2][index]=CheckScore(req.body.chentianlong);
-  scores[3][index]=CheckScore(req.body.chengyao);
-  scores[4][index]=CheckScore(req.body.dengqiliang);
-  scores[5][index]=CheckScore(req.body.fujinxiang);
-  scores[6][index]=CheckScore(req.body.guyichang);
-  scores[7][index]=CheckScore(req.body.lixingbin);
-  scores[8][index]=CheckScore(req.body.lvyi);
-  scores[9][index]=CheckScore(req.body.wangdongjie);
-  scores[10][index]=CheckScore(req.body.xuwei);
-  scores[11][index]=CheckScore(req.body.yehong);
-  scores[12][index]=CheckScore(req.body.zhangqiang);
-  scores[13][index]=CheckScore(req.body.zhulongfei);
-  Statistic();
-}
-var a=CountFinishUser();
-if(a>=2){
- res.render('statistics', {
-        users: users,
-        scores: scores,
-        statisticscores: statisticscores,
-        statisticscoretitles: statisticscoretitles,
-    });
-}
-else{
-res.render('submiting', {
-        users: users,
-        scores: scores,
-        statisticscores: statisticscores,
-        statisticscoretitles: statisticscoretitles,
-    });
-}
+  Load(res);
 });
 
-function CheckScore(score){
-  if(score=="") {
-    return 0;}
-    else{
-      return score;
-    }
+router.get('/statistics.html', function (req, res, next) {
+  Load(res);
+});
+
+function Load(res) {
+  var finishusercount = CountFinishUser();
+  if (CountFinishUser() >= 5) {
+    res.render('statistics', {
+      title: title,
+      users: users,
+      scores: scores,
+    });
+  }
+  else {
+    res.render('submiting', {
+      users: users,
+      finishusercount: finishusercount,
+    });
+  }
 }
 
-function CountFinishUser(){
-  var count=0;
-  scores[0].forEach(function(score,i) {
-    if(~~score > 10){
+function CheckScore(score) {
+  if (score == "") {
+    return 0;
+  }
+  else {
+    return ~~score;
+  }
+}
+
+function CountFinishUser() {
+  var count = 0;
+  for (i = 0; i < users.length; i++) {
+    if (~~scores[0][i] > 0) {
       count++;
     }
-  }, this);
+  }
   return count;
 }
 
-function Statistic(){
-  var totle=0;
-  scores.forEach(function(everyonescore,i) {
-    everyonescore.forEach(function(score) {
-      totle+=(~~score);
-    }, this);
-    statisticscores[i][0]=totle;
-    statisticscores[i][1]=(totle/scores.length).toFixed(1);
-    totle=0;
+function Statistic() {
+  var totle = 0;
+  scores.forEach(function (everyonescore, i) {
+    for (j = 0; j < users.length; j++) {
+      totle += ~~everyonescore[j];
+    }
+    scores[i][14] = totle;
+    scores[i][15] = (totle / scores.length).toFixed(1);
+    totle = 0;
   }, this);
 }
-
-router.get('/statistics.html', function(req, res, next) {
-var a=CountFinishUser();
-if(a>=2){
- res.render('statistics', {
-        users: users,
-        scores: scores,
-        statisticscores: statisticscores,
-        statisticscoretitles: statisticscoretitles,
-    });
-}
-else{
-res.render('submiting', {
-        users: users,
-        scores: scores,
-        statisticscores: statisticscores,
-        statisticscoretitles: statisticscoretitles,
-    });
-}
-});
 
 module.exports = router;
